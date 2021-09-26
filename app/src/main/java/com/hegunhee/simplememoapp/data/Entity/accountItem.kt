3361a3bb820a82abb43202001ea47071f5f1eac6
gunhe <leech9876@naver.com>
2021-09-26 21:48:37 +0900

    accountItemAdapter 기초 설계

---
+++ app/src/main/java/com/hegunhee/simplememoapp/data/Entity/accountItem.kt
@@ -0,0 +1,10 @@
+package com.hegunhee.simplememoapp.data.Entity
+
+data class accountItem(
+    val category : String,
+    val description : String?,
+    val day : String,
+    val time : String,
+    val price : Int,
+    val payType : String
+)
---
+++ app/src/main/java/com/hegunhee/simplememoapp/presentation/adapter/AccountItemViewAdapter.kt
@@ -0,0 +1,40 @@
+package com.hegunhee.simplememoapp.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hegunhee.simplememoapp.data.Entity.accountItem
+import com.hegunhee.simplememoapp.databinding.AccountitemviewBinding
+
+class AccountItemViewAdapter : RecyclerView.Adapter<AccountItemViewAdapter.AccountItemViewHolder>() {
+
+    private var accountList : List<accountItem> = listOf()
+
+    inner class AccountItemViewHolder(private val binding : AccountitemviewBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bindView(item : accountItem) = with(binding){
+            this.description.text = item.description ?: item.payType
+            //
+            //
+
+            this.root.setOnClickListener {
+                // 클릭할시 세부화면으로 이동
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccountItemViewHolder {
+        val view = AccountitemviewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return AccountItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AccountItemViewHolder, position: Int) {
+        holder.bindView(accountList[position])
+
+    }
+
+    override fun getItemCount(): Int = accountList.size
+
+    fun setData(itemList : List<accountItem>){
+        accountList = itemList
+    }
+}
