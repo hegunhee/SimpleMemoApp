c4dfff3c009436afecf62837ab53ee6fcc8177e8
gunhe <leech9876@naver.com>
2021-10-03 23:03:15 +0900

    MemoActivity XML draw

--- app/src/main/java/com/hegunhee/simplememoapp/presentation/Main/MainState.kt
+++ app/src/main/java/com/hegunhee/simplememoapp/presentation/Main/MainState.kt
@@ -1,7 +1,11 @@
 package com.hegunhee.simplememoapp.presentation.Main
 
+import com.hegunhee.simplememoapp.data.Entity.accountItem
+
 sealed class MainState{
     object Uninitalized : MainState()
 
     object Loading : MainState()
+
+    data class Success(var ItemList : List<accountItem>) : MainState()
 }
--- app/src/main/java/com/hegunhee/simplememoapp/presentation/addMemo/AddMemoBetaActivity.kt
+++ app/src/main/java/com/hegunhee/simplememoapp/presentation/addMemo/AddMemoBetaActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import com.hegunhee.simplememoapp.data.Entity.accountItem
 import com.hegunhee.simplememoapp.databinding.AddMemoBetaBinding
 import com.hegunhee.simplememoapp.presentation.Main.MainActivity
@@ -14,35 +16,70 @@ import java.time.LocalTime
 
 class AddMemoBetaActivity : AppCompatActivity() {
 
-    private lateinit var binding : AddMemoBetaBinding
+    private lateinit var binding: AddMemoBetaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = AddMemoBetaBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initViews()
     }
-    private fun initViews() = with(binding){
+
+    private fun initViews() = with(binding) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             day.text = LocalDate.now().toString()
             time.text = LocalTime.now().toString()
         }
-
+        income.setOnClickListener {
+            category.text = "수입"
+        }
+        expenses.setOnClickListener {
+            category.text = "지출"
+        }
         save.setOnClickListener {
-            Intent(this@AddMemoBetaActivity,MainActivity::class.java)?.apply {
-                putExtra(Item,accountItem(
-                    category = "카테고리",
-                    description = "설명",
-                    day = "날짜",
-                    time = "시간",
-                    price = 20000,
-                    payType = "카드"
-                ))
-                setResult(RESULT_OK,this)
+            Intent(this@AddMemoBetaActivity, MainActivity::class.java)?.apply {
+                putExtra(
+                    Item, accountItem(
+                        category = "카테고리",
+                        description = "설명",
+                        day = "날짜",
+                        time = "시간",
+                        price = 20000,
+                        payType = "카드"
+                    )
+                )
+                setResult(RESULT_OK, this)
                 finish()
             }
         }
+        asset.setOnClickListener {
+            Toast.makeText(this@AddMemoBetaActivity, "자산 테이블 호출.", Toast.LENGTH_SHORT).show()
+            assetTable.isVisible = true
+
+        }
+        attr.setOnClickListener {
+            Toast.makeText(this@AddMemoBetaActivity, "분류 테이블 호출", Toast.LENGTH_SHORT).show()
+        }
+
+        back.setOnClickListener {
+            onBackPressed()
+        }
+
+        assetTableBackButton.setOnClickListener {
+            assetTable.isGone = true
+        }
+        assetCard.setOnClickListener {
+            asset.text = "카드"
+        }
+        assetCash.setOnClickListener {
+            asset.text = "현금"
+        }
+        assetKakaoBank.setOnClickListener {
+            asset.text = "카카오뱅크"
+        }
     }
-    companion object{
+
+
+    companion object {
         const val Item = "accountItem"
     }
 }
