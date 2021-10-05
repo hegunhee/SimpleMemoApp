ebcc7470e0b269cd5300eff289e7ed3666308ad0
gunhe <leech9876@naver.com>
2021-10-05 21:51:38 +0900

    날짜와 시간 설정을 마침

--- app/src/main/java/com/hegunhee/simplememoapp/presentation/addMemo/AddMemoBetaActivity.kt
+++ app/src/main/java/com/hegunhee/simplememoapp/presentation/addMemo/AddMemoBetaActivity.kt
@@ -3,8 +3,8 @@ package com.hegunhee.simplememoapp.presentation.addMemo
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
@@ -12,8 +12,10 @@ import androidx.core.view.isVisible
 import com.hegunhee.simplememoapp.data.Entity.accountItem
 import com.hegunhee.simplememoapp.databinding.AddMemoBetaBinding
 import com.hegunhee.simplememoapp.presentation.Main.MainActivity
-import java.time.LocalDate
-import java.time.LocalTime
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
 import java.util.*
 
 class AddMemoBetaActivity : AppCompatActivity() {
@@ -27,10 +29,7 @@ class AddMemoBetaActivity : AppCompatActivity() {
     }
 
     private fun initViews() = with(binding) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            day.text = LocalDate.now().toString()
-            time.text = LocalTime.now().toString()
-        }
+        setCurrentTime()
 
         back.setOnClickListener {
             onBackPressed()
@@ -70,16 +69,19 @@ class AddMemoBetaActivity : AppCompatActivity() {
             val asset = if (asset.text.isNullOrEmpty()) {
                 Toast.makeText(this@AddMemoBetaActivity, "자산을 선택해주세요", Toast.LENGTH_SHORT)
                     .show()
+                asset.callOnClick()
                 return@setOnClickListener
             } else asset.text.toString()
 
             val attr = if (attr.text.isNullOrEmpty()) {
                 Toast.makeText(this@AddMemoBetaActivity, "분류를 선택해주세요", Toast.LENGTH_SHORT)
                     .show()
+                attr.callOnClick()
                 return@setOnClickListener
             } else attr.text.toString()
             val price = if (price.text.isNullOrEmpty()) {
                 Toast.makeText(this@AddMemoBetaActivity, "가격을 입력해주세요", Toast.LENGTH_SHORT).show()
+                price.callOnClick()
                 return@setOnClickListener
             } else price.text.toString().toInt()
             val description = description.text.toString()
@@ -109,11 +111,10 @@ class AddMemoBetaActivity : AppCompatActivity() {
         }
 
         day.setOnClickListener {
-            val cal = Calendar.getInstance()
             DatePickerDialog(
                 this@AddMemoBetaActivity,
                 DatePickerDialog.OnDateSetListener { datePicker, y, m, d ->
-                    day.text = "$y:${m+1}:$d"
+                    day.text = "${y}/${m + 1}/$d (${getDayOfWeek("$y-${m+1}-${d}")})"
                 },
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
@@ -121,11 +122,10 @@ class AddMemoBetaActivity : AppCompatActivity() {
             ).show()
         }
         time.setOnClickListener {
-            val cal = Calendar.getInstance()
             TimePickerDialog(
                 this@AddMemoBetaActivity,
                 TimePickerDialog.OnTimeSetListener { timePicker, h, m ->
-                    time.text = "$h:$m"
+                    time.text = "${ampmTime(h)}:$m"
                 },
                 cal.get(Calendar.HOUR),
                 cal.get(Calendar.MINUTE),
@@ -195,6 +195,43 @@ class AddMemoBetaActivity : AppCompatActivity() {
 
     }
 
+    private fun setCurrentTime() = with(binding) {
+        val dayText =
+            "${cal.get(Calendar.YEAR)}/${cal.get(Calendar.MONTH) + 1}/${cal.get(Calendar.DAY_OF_MONTH)}(${getDefaultDayOfWeek()}) "
+        day.text = dayText
+        val timeText =
+            if (cal.get(Calendar.HOUR_OF_DAY) > 12) "오후 ${cal.get(Calendar.HOUR_OF_DAY) - 12}:${cal.get(Calendar.MINUTE)}" else "오전 ${
+                cal.get(Calendar.HOUR_OF_DAY)
+            }:${cal.get(Calendar.MINUTE)}"
+        time.text = timeText
+    }
+    private fun ampmTime(time : Int) : String {
+        return if(time> 12){
+            "오후 ${time-12}"
+        }else{
+            "오전 $time"
+        }
+    }
+
+    private fun getDayOfWeek(date : String) : String {
+        val df = SimpleDateFormat("yyyy-MM-dd")
+        val nDate = df.parse(date)
+        val specificCal = Calendar.getInstance().apply { this.time = nDate }
+        return getDefaultDayOfWeek(specificCal)
+    }
+    private fun getDefaultDayOfWeek(calender : Calendar = cal): String {
+        Log.d("Default",""+calender.time)
+        return when (calender.get(Calendar.DAY_OF_WEEK)) {
+            1 -> "일"
+            2 -> "월"
+            3 -> "화"
+            4 -> "수"
+            5 -> "목"
+            6 -> "금"
+            7 -> "토"
+            else -> "null"
+        }
+    }
     private fun toggleTableVisiblity(category: String) = with(binding) {
         when (category) {
             "asset" -> {
@@ -222,5 +259,6 @@ class AddMemoBetaActivity : AppCompatActivity() {
 
     companion object {
         const val Item = "accountItem"
+        val cal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"))
     }
 }
