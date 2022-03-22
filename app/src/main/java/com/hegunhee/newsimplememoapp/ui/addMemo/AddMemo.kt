ed9e755f7cb5c0473a9e80f31c9d4a37a83b1c21
hegunhee <gunhee0072@gmail.com>
2022-03-22 23:15:17 +0900

    DatePicker와 dayPicker를 사용하였습니다.

--- app/src/main/java/com/hegunhee/newsimplememoapp/ui/addMemo/AddMemo.kt
+++ app/src/main/java/com/hegunhee/newsimplememoapp/ui/addMemo/AddMemo.kt
@@ -1,5 +1,7 @@
 package com.hegunhee.newsimplememoapp.ui.addMemo
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,30 +9,32 @@ import androidx.databinding.DataBindingUtil
 import com.hegunhee.newsimplememoapp.R
 import com.hegunhee.newsimplememoapp.databinding.ActivityAddMemoBinding
 import org.koin.android.ext.android.inject
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 
 class AddMemo : AppCompatActivity() {
 
-    private lateinit var binding : ActivityAddMemoBinding
-    private val addMemoViewModel : AddMemoViewModel by inject()
+    private lateinit var binding: ActivityAddMemoBinding
+    private val addMemoViewModel: AddMemoViewModel by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_add_memo)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_memo)
         binding.viewmodel = addMemoViewModel
         binding.lifecycleOwner = this
         addMemoViewModel.initData()
         initListener()
     }
 
-    private fun initListener(){
+    private fun initListener() {
         binding.backButton.setOnClickListener {
             onBackPressed()
         }
         binding.day.setOnClickListener {
-            Toast.makeText(this, "day", Toast.LENGTH_SHORT).show()
+            setDate()
         }
         binding.time.setOnClickListener {
-            Toast.makeText(this, "time", Toast.LENGTH_SHORT).show()
+            setTime()
         }
         binding.asset.setOnClickListener {
             Toast.makeText(this, "asset", Toast.LENGTH_SHORT).show()
@@ -39,5 +43,37 @@ class AddMemo : AppCompatActivity() {
             Toast.makeText(this, "attr", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    private fun setDate(){
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year,month,dayOfMonth ->
+            addMemoViewModel.setDate(LocalDate.of(year,month+1,dayOfMonth))
+        }
+        with(addMemoViewModel){
+            DatePickerDialog(this@AddMemo,dateSetListener,year,month-1,day).show()
+            Toast.makeText(this@AddMemo, "${year}년${month}월${day}일 ${day_of_week}", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun setTime() {
+        val time = LocalDateTime.now().plusHours(9)
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+            with(addMemoViewModel) {
+                if (hourOfDay > 12) {
+                    ampm = "오후"
+                    hour = hourOfDay - 12
+                    this.minute = minute
+                } else {
+                    ampm = "오전"
+                    hour = hourOfDay
+                    this.minute = minute
+                }
+                Toast.makeText(this@AddMemo, "${ampm},${hour}:${minute}", Toast.LENGTH_SHORT).show()
+                setTimeInfo()
+            }
+
+        }
+        TimePickerDialog(this, timeSetListener, time.hour, time.minute, false).show()
     }
 }
--- app/src/main/java/com/hegunhee/newsimplememoapp/ui/addMemo/AddMemoViewModel.kt
+++ app/src/main/java/com/hegunhee/newsimplememoapp/ui/addMemo/AddMemoViewModel.kt
@@ -10,27 +10,29 @@ class AddMemoViewModel : ViewModel() {
 
     val category = MutableLiveData<String>()
 
-    var year : Int = 0
-    var month : Int = 0
-    var day : Int = 0
-    var day_of_week : String = ""
+    var year: Int = 0
+    var month: Int = 0
+    var day: Int = 0
+    var day_of_week: String = ""
     val date_Info = MutableLiveData<String>()
 
-    var ampm : String = ""
-    var hour : Int = 0
-    var minute : Int = 0
+    var ampm: String = ""
+    var hour: Int = 0
+    var minute: Int = 0
     var time_Info = MutableLiveData<String>()
 
-    fun initData(){
+    fun initData() {
         category.value = "지출"
         setDate()
-        setTime()
+        initTime()
 
     }
 
 
+    //Dialog로 데이터를 받아서 사용할때는 LocalDate.of를 사용하자
 
-    fun setDate(date : LocalDate = LocalDate.now()){
+    fun setDate(date: LocalDate = LocalDate.now()) {
+
         year = date.year
         month = date.monthValue
         day = date.dayOfMonth
@@ -38,35 +40,40 @@ class AddMemoViewModel : ViewModel() {
         date_Info.value = "${year}/${month}/${day} (${day_of_week})"
     }
 
-    fun setTime(day : LocalDateTime = LocalDateTime.now()){
-        if(day.hour > 12){
+    private fun initTime() {
+        val day = LocalDateTime.now().plusHours(9)
+        if (day.hour > 12) {
             ampm = "오후"
             hour = day.hour - 12
             minute = day.minute
-        }else{
+        } else {
             ampm = "오전"
             hour = day.hour
             minute = day.minute
         }
+        setTimeInfo()
+    }
+
+    fun setTimeInfo(){
         time_Info.value = "$ampm ${hour}:${minute}"
     }
 
-    fun setCategoryIncome(){
+    fun setCategoryIncome() {
         category.value = "수입"
     }
 
-    fun setCategoryExpense(){
+    fun setCategoryExpense() {
         category.value = "지출"
     }
 
-    private fun transferdayofweekbyKorean(day_of_week : String) : String{
-        return when(day_of_week){
+    private fun transferdayofweekbyKorean(day_of_week: String): String {
+        return when (day_of_week) {
             "MONDAY" -> "월"
             "TUESDAY" -> "화"
             "WEDNESDAY" -> "수"
             "THURSDAY" -> "목"
             "FRIDAY" -> "금"
-            "SATURDAY"-> "토"
+            "SATURDAY" -> "토"
             "SUNDAY" -> "일"
             else -> "error"
         }
