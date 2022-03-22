54bd3b6309ac080966f35ac717eabc2cecd5a86c
hegunhee <gunhee0072@gmail.com>
2022-03-22 22:39:47 +0900

    xml을 추가하고 날짜, 시간을 초기화하는 로직을 구현하였습니다.

--- app/src/main/java/com/hegunhee/newsimplememoapp/ui/addMemo/AddMemo.kt
+++ app/src/main/java/com/hegunhee/newsimplememoapp/ui/addMemo/AddMemo.kt
@@ -2,6 +2,7 @@ package com.hegunhee.newsimplememoapp.ui.addMemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.hegunhee.newsimplememoapp.R
 import com.hegunhee.newsimplememoapp.databinding.ActivityAddMemoBinding
@@ -17,6 +18,7 @@ class AddMemo : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_add_memo)
         binding.viewmodel = addMemoViewModel
         binding.lifecycleOwner = this
+        addMemoViewModel.initData()
         initListener()
     }
 
@@ -24,5 +26,18 @@ class AddMemo : AppCompatActivity() {
         binding.backButton.setOnClickListener {
             onBackPressed()
         }
+        binding.day.setOnClickListener {
+            Toast.makeText(this, "day", Toast.LENGTH_SHORT).show()
+        }
+        binding.time.setOnClickListener {
+            Toast.makeText(this, "time", Toast.LENGTH_SHORT).show()
+        }
+        binding.asset.setOnClickListener {
+            Toast.makeText(this, "asset", Toast.LENGTH_SHORT).show()
+        }
+        binding.attr.setOnClickListener {
+            Toast.makeText(this, "attr", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
--- app/src/main/java/com/hegunhee/newsimplememoapp/ui/addMemo/AddMemoViewModel.kt
+++ app/src/main/java/com/hegunhee/newsimplememoapp/ui/addMemo/AddMemoViewModel.kt
@@ -2,9 +2,73 @@ package com.hegunhee.newsimplememoapp.ui.addMemo
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
+import java.time.LocalDateTime
+import kotlin.math.min
 
 class AddMemoViewModel : ViewModel() {
 
-    val category : String = "지출"
+    val category = MutableLiveData<String>()
 
+    var year : Int = 0
+    var month : Int = 0
+    var day : Int = 0
+    var day_of_week : String = ""
+    val date_Info = MutableLiveData<String>()
+
+    var ampm : String = ""
+    var hour : Int = 0
+    var minute : Int = 0
+    var time_Info = MutableLiveData<String>()
+
+    fun initData(){
+        category.value = "지출"
+        setDate()
+        setTime()
+
+    }
+
+
+
+    fun setDate(date : LocalDate = LocalDate.now()){
+        year = date.year
+        month = date.monthValue
+        day = date.dayOfMonth
+        day_of_week = transferdayofweekbyKorean(date.dayOfWeek.toString())
+        date_Info.value = "${year}/${month}/${day} (${day_of_week})"
+    }
+
+    fun setTime(day : LocalDateTime = LocalDateTime.now()){
+        if(day.hour > 12){
+            ampm = "오후"
+            hour = day.hour - 12
+            minute = day.minute
+        }else{
+            ampm = "오전"
+            hour = day.hour
+            minute = day.minute
+        }
+        time_Info.value = "$ampm ${hour}:${minute}"
+    }
+
+    fun setCategoryIncome(){
+        category.value = "수입"
+    }
+
+    fun setCategoryExpense(){
+        category.value = "지출"
+    }
+
+    private fun transferdayofweekbyKorean(day_of_week : String) : String{
+        return when(day_of_week){
+            "MONDAY" -> "월"
+            "TUESDAY" -> "화"
+            "WEDNESDAY" -> "수"
+            "THURSDAY" -> "목"
+            "FRIDAY" -> "금"
+            "SATURDAY"-> "토"
+            "SUNDAY" -> "일"
+            else -> "error"
+        }
+    }
 }
