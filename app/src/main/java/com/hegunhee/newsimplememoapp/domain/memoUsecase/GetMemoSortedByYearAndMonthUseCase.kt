8436200d20cb29750786cad9190eaf35eb9fe6f8
hegunhee <gunhee0072@gmail.com>
2022-04-08 19:18:21 +0900

    정렬 순서를 수정하였습니다.

--- app/src/main/java/com/hegunhee/newsimplememoapp/domain/memoUsecase/GetMemoSortedByYearAndMonthUseCase.kt
+++ app/src/main/java/com/hegunhee/newsimplememoapp/domain/memoUsecase/GetMemoSortedByYearAndMonthUseCase.kt
@@ -7,7 +7,11 @@ import com.hegunhee.newsimplememoapp.model.MemoRepository
 class GetMemoSortedByYearAndMonthUseCase(
     private val repository: MemoRepository
 ) : UseCase {
-    suspend operator fun invoke(year : Int,month : Int) : List<Memo>{
-        return repository.getMemoListSortedByYearAndMonth(year,month)
+    suspend operator fun invoke(year: Int, month: Int): List<Memo> {
+        return repository.getMemoListSortedByYearAndMonth(year, month)
+            .sortedByDescending { it.minute }.sortedByDescending {
+                it.hour + if (it.amPm == "오후") 12 else 0
+            }.sortedByDescending { it.day }
+
     }
 }
