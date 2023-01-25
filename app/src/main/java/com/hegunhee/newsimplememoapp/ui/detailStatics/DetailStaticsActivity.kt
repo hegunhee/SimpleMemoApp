bf05b4727cd68f6dbe2f0cb59c3812bd9d08d0c2
이건희 <gunhee0072@gmail.com>
2023-01-25 16:42:26 +0900

    [MEMO-REFACTOR] 당분간 사용 안할 Activity MemoAdapter 주석처리

--- app/src/main/java/com/hegunhee/newsimplememoapp/ui/detailStatics/DetailStaticsActivity.kt
+++ app/src/main/java/com/hegunhee/newsimplememoapp/ui/detailStatics/DetailStaticsActivity.kt
@@ -12,17 +12,20 @@ import com.hegunhee.newsimplememoapp.ui.detailMemo.DetailMemoActivity
 import com.hegunhee.newsimplememoapp.ui.common.MemoAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * Adapter의 경우 다시
+ */
 @AndroidEntryPoint
 class DetailStaticsActivity : AppCompatActivity() {
 
     private val viewModel: DetaiStaticsViewModel by viewModels()
     private lateinit var binding: ActivityDetailStaticsBinding
-    private val adapter = MemoAdapter() { memo ->
-        Intent(this, DetailMemoActivity::class.java).apply {
-            putExtra("Memo", memo)
-            this@DetailStaticsActivity.startActivity(this)
-        }
-    }
+//    private val adapter = MemoAdapter() { memo ->
+//        Intent(this, DetailMemoActivity::class.java).apply {
+//            putExtra("Memo", memo)
+//            this@DetailStaticsActivity.startActivity(this)
+//        }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +33,7 @@ class DetailStaticsActivity : AppCompatActivity() {
         binding.run {
             viewmodel = this@DetailStaticsActivity.viewModel
             lifecycleOwner = this@DetailStaticsActivity
-            recyclerview.adapter = adapter
+//            recyclerview.adapter = adapter
         }
         viewModel.initData(navArgs<DetailStaticsActivityArgs>().value.staticsData)
         initObserver()
@@ -47,15 +50,15 @@ class DetailStaticsActivity : AppCompatActivity() {
     }
 
     private fun initObserverData() = viewModel.detailStaticsState.observe(this) {
-        when (it) {
-            DetailStaticsState.Uninitialized -> {}
-            is DetailStaticsState.Success -> {
-                adapter.submitList(it.data)
-            }
-            DetailStaticsState.NullOrEmpty -> {
-
-            }
-        }
+//        when (it) {
+//            DetailStaticsState.Uninitialized -> {}
+//            is DetailStaticsState.Success -> {
+//                adapter.submitList(it.data)
+//            }
+//            DetailStaticsState.NullOrEmpty -> {
+//
+//            }
+//        }
     }
 
     private fun initBackButtonObserve() = viewModel.backButton.observe(this) {
