30d7b5957bf35ed358a7ec5ad11b62dc5da826ba
이건희 <gunhee0072@gmail.com>
2023-01-26 11:43:41 +0900

    [MEMO-REFACTOR] 메모 날짜 xml 완성 및 ViewHolder 완성

--- app/src/main/java/com/hegunhee/newsimplememoapp/ui/common/MemoAdapter.kt
+++ app/src/main/java/com/hegunhee/newsimplememoapp/ui/common/MemoAdapter.kt
@@ -26,8 +26,13 @@ class MemoAdapter(val actionHandler : MemoAdapterActionHandler) :
 
     inner class MemoDateViewHolder(private val binding : ItemMemoDateBinding) : MemoAdapterViewHolder(binding.root){
 
-        override fun bindView(memo : MemoType){
-
+        override fun bindView(memo : MemoType) : Unit = with(binding){
+            val memoDate = memo as MemoType.MemoDate
+            this.day.text = memoDate.day.toString()
+            this.dayOfWeek.text = memoDate.dayOfWeek +"요일"
+            this.yearAndMonth.text = "" + memoDate.year + "." + memoDate.month
+            this.income.text = memoDate.incomeSum.toString() + "원"
+            this.expense.text = memoDate.expenseSum.toString() + "원"
         }
 
     }
