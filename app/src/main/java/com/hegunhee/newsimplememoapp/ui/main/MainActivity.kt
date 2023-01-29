86da8b85ca82d724905b5b228dcf8b40cf3b3747
이건희 <gunhee0072@gmail.com>
2023-01-29 09:44:13 +0900

    [MAIN-FRAGMENT] MainActivity의 화면을 MainFragment로 옮김

--- app/src/main/java/com/hegunhee/newsimplememoapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/hegunhee/newsimplememoapp/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hegunhee.newsimplememoapp.ui
+package com.hegunhee.newsimplememoapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -19,9 +19,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_main_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        with(binding){
-            bottomNavigation.setupWithNavController(navController)
-        }
 
         // FIXME : 다크모드 정식 오픈시 해당 코드 제거
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
