9eb64ec8b6a11cf7e143c4972befea32045c4e33
이건희 <gunhee0072@gmail.com>
2023-02-07 09:58:40 +0900

    [DOMAIN-MODULE] 도메인 모듈을 순수 자바 모듈로 변경

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,50 +1,15 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-
-    id ("kotlin-parcelize")
-    id ("kotlin-kapt")
-    id ("dagger.hilt.android.plugin")
-
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
 }
-
-android {
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+java{
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
 
-    implementation(libs.core.ktx)
-
-    // Coroutines
-    implementation(libs.bundles.coroutine)
-    implementation(libs.coroutine.test)
-    // Hilt
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.compiler)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.javax.inject)
+    implementation(libs.coroutine.core)
 }
