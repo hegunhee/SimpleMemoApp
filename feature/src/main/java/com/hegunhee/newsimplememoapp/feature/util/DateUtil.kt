0294d66b01a006bc9f01a9f245643ecc843211dc
이건희 <57277631+hegunhee@users.noreply.github.com>
2023-09-05 23:28:54 +0900

    Revert "[android8.0-time-support] feat : android 8.0 이전 버전에서 사용하는 시간 변수 설정"
    This reverts commit 6880b46482b81bcff8e5738af4f67df1b3886d1b.

--- feature/src/main/java/com/hegunhee/newsimplememoapp/feature/util/DateUtil.kt
+++ feature/src/main/java/com/hegunhee/newsimplememoapp/feature/util/DateUtil.kt
@@ -1,13 +1,5 @@
 package com.hegunhee.newsimplememoapp.feature.util
 
-import java.util.Calendar
-import java.util.Date
-
 object DateUtil {
 
-    private val calendar = Calendar.getInstance().apply {
-        time = Date()
-        add(Calendar.HOUR_OF_DAY,9)
-        add(Calendar.MONTH,1)
-    }
 }
