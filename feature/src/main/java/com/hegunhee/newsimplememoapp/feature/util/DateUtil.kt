5d3277d281d31a1e95c0d31ae08275285b43db6a
이건희 <57277631+hegunhee@users.noreply.github.com>
2023-09-05 23:28:54 +0900

    Revert "[android8.0-time-support] feat : 요일 가져오는 함수 작성"
    This reverts commit fbfd71385f223b41926678c37b9a6680c7402454.

--- feature/src/main/java/com/hegunhee/newsimplememoapp/feature/util/DateUtil.kt
+++ feature/src/main/java/com/hegunhee/newsimplememoapp/feature/util/DateUtil.kt
@@ -2,15 +2,10 @@ package com.hegunhee.newsimplememoapp.feature.util
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import java.text.SimpleDateFormat
-import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
 import java.time.ZoneId
 import java.util.Calendar
 import java.util.Date
-import java.util.Locale
-import kotlin.math.abs
 
 object DateUtil {
 
@@ -21,8 +16,6 @@ object DateUtil {
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun getKoreaLocalTime() = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
-
-    private val koreanDayOfWeek = listOf<String>("일","월","화","수","목","금","토")
 
     fun getYear() : Int {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -38,46 +31,6 @@ object DateUtil {
         }else {
             calendar.get(Calendar.MONTH)
         }
-    }
-    fun getDayOfMonth(): Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            getKoreaLocalTime().dayOfMonth
-        }else {
-            calendar.get(Calendar.DAY_OF_MONTH)
-        }
-    }
-    fun getDayOfWeek(year : Int,month : Int,day : Int) : String {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LocalDate.of(year,month,day).dayOfWeek.toString().changeKoreanDayOfWeek()
-        } else {
-            val diffDay = getDiffDay(year,month,day)
-            return if (diffDay >= 0) {
-                koreanDayOfWeek[(calendar.get(Calendar.DAY_OF_WEEK) - 1 + diffDay) % 7]
-            } else {
-                koreanDayOfWeek[abs(calendar.get(Calendar.DAY_OF_WEEK) - 1 + (7-(diffDay%7))) % 7]
-            }
-        }
-    }
-
-    private fun String.changeKoreanDayOfWeek(): String =
-        when (this) {
-            "MONDAY" -> "월"
-            "TUESDAY" -> "화"
-            "WEDNESDAY" -> "수"
-            "THURSDAY" -> "목"
-            "FRIDAY" -> "금"
-            "SATURDAY" -> "토"
-            "SUNDAY" -> "일"
-            else -> "error"
-        }
-
-    private fun getDiffDay(year : Int,month : Int,day : Int) : Int {
-        val todayYear = getYear()
-        val todayMonth = getMonth()
-        val todayDay = getDayOfMonth()
-        val today = ("" + todayYear + todayMonth + todayDay).toInt()
-        val selectDay = ("" + year + month + day).toInt()
-        return  today - selectDay
     }
 
     fun getHour() : Int {
