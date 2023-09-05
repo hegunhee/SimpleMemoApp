2a0bd19261868b85744901f7d063941835edc03f
이건희 <57277631+hegunhee@users.noreply.github.com>
2023-09-05 23:28:54 +0900

    Revert "[android8.0-time-support] feat : 연도와 달을 제공하는 함수 작성"
    This reverts commit ffb6914808236febe1b082e166b22612ba6d0bc5.

--- feature/src/main/java/com/hegunhee/newsimplememoapp/feature/util/DateUtil.kt
+++ feature/src/main/java/com/hegunhee/newsimplememoapp/feature/util/DateUtil.kt
@@ -1,7 +1,5 @@
 package com.hegunhee.newsimplememoapp.feature.util
 
-import android.os.Build
-import java.time.LocalDate
 import java.util.Calendar
 import java.util.Date
 
@@ -11,21 +9,5 @@ object DateUtil {
         time = Date()
         add(Calendar.HOUR_OF_DAY,9)
         add(Calendar.MONTH,1)
-    }
-
-    fun getYear() : Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LocalDate.now().year
-        }else {
-            calendar.get(Calendar.YEAR)
-        }
-    }
-
-    fun getMonth() : Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LocalDate.now().monthValue
-        }else {
-            calendar.get(Calendar.MONTH)
-        }
     }
 }
