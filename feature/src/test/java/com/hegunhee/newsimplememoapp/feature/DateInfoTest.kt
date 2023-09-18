477afb66accdb1111d151f0ac0e9cf9c1a500e15
hegunhee <gunhee0072@gmail.com>
2023-09-18 19:15:03 +0900

    [memo-category-dialog] refactor : 메모 추가시 날짜 관련 변수들을 하나의 클래스로 변경

---
+++ feature/src/test/java/com/hegunhee/newsimplememoapp/feature/DateInfoTest.kt
@@ -0,0 +1,20 @@
+package com.hegunhee.newsimplememoapp.feature
+
+import com.hegunhee.newsimplememoapp.feature.common.DateInfo
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DateInfoTest {
+
+    @Test
+    fun `date info dateStamp Test`() {
+        val year = 2023
+        val month = 9
+        val day = 18
+        val dayOfWeek = "월"
+        val dateStamp = "${year}/${month}/${day} (${dayOfWeek})"
+        val dateInfo = DateInfo(year,month,day,dayOfWeek)
+        println(dateInfo.dateStamp)
+        assertEquals(dateInfo.dateStamp,dateStamp)
+    }
+}
