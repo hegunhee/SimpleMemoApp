e5dad8d532a197daa8bd2725971125d2c8fb5ad5
hegunhee <gunhee0072@gmail.com>
2023-09-24 10:56:05 +0900

    [memo-category-DB] feat : entity와 model을 전환하는 매퍼 패키지 생성

--- data/src/main/java/com/hegunhee/newsimplememoapp/data/entity/MemoEntity.kt
+++ data/src/main/java/com/hegunhee/newsimplememoapp/data/entity/MemoEntity.kt
@@ -3,7 +3,6 @@ package com.hegunhee.newsimplememoapp.data.entity
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.hegunhee.newsimplememoapp.domain.model.MemoType
 import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "Memo")
@@ -23,14 +22,4 @@ data class MemoEntity(
     val description: String,
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
-) : Parcelable {
-
-    fun toMemo() : MemoType.Memo {
-        return MemoType.Memo(category, year, month, day, dayOfWeek, amPm, hour, minute, attr, price, asset, description, id)
-    }
-}
-
-
-fun MemoType.Memo.toMemoEntity() : MemoEntity{
-    return MemoEntity(category, year, month, day, dayOfWeek, amPm, hour, minute, attr, price, asset, description, id)
-}
+) : Parcelable
