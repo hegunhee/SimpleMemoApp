ebb2bba8728ceb50c03ef4ced19faf4be7f06f83
hegunhee <gunhee0072@gmail.com>
2023-09-24 13:21:59 +0900

    [memo-category-DB] feat : 특정 타입에 해당 카테고리가 있는지 검사하는 코드 추가

---
+++ domain/src/main/java/com/hegunhee/newsimplememoapp/domain/usecase/CheckIsCategoryUseCase.kt
@@ -0,0 +1,12 @@
+package com.hegunhee.newsimplememoapp.domain.usecase
+
+import com.hegunhee.newsimplememoapp.domain.model.CategoryType
+import com.hegunhee.newsimplememoapp.domain.repository.MemoRepository
+import javax.inject.Inject
+
+class CheckIsCategoryUseCase @Inject constructor(private val repository: MemoRepository) {
+
+    suspend operator fun invoke(categoryType: CategoryType,text : String) :Boolean{
+        return repository.checkIsCategory(categoryType,text)
+    }
+}
