7ddeda59a22a4805f07c5bdb05a0e69c4b62b8f7
hegunhee <gunhee0072@gmail.com>
2023-09-24 12:56:47 +0900

    [memo-category-DB] docs : CategoryType domain모듈에서 data모듈로 이동

--- feature/src/main/java/com/hegunhee/newsimplememoapp/feature/common/category/CategoryActionHandler.kt
+++ feature/src/main/java/com/hegunhee/newsimplememoapp/feature/common/category/CategoryActionHandler.kt
@@ -1,5 +1,7 @@
 package com.hegunhee.newsimplememoapp.feature.common.category
 
+import com.hegunhee.newsimplememoapp.domain.model.CategoryType
+
 interface CategoryActionHandler {
 
     fun onBottomSheetDismiss()
