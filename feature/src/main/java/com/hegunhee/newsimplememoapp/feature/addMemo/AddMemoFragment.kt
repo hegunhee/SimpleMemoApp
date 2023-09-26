8dc1fa23cc80285b577ca6c3e6cdd78013d984fd
hegunhee <gunhee0072@gmail.com>
2023-09-26 17:10:14 +0900

    [memo-category-dialog] feat : AddMemo와 DetailMemo에서 카테고리 추가로 이동하도록 설정

--- feature/src/main/java/com/hegunhee/newsimplememoapp/feature/addMemo/AddMemoFragment.kt
+++ feature/src/main/java/com/hegunhee/newsimplememoapp/feature/addMemo/AddMemoFragment.kt
@@ -11,12 +11,15 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.fragment.app.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.hegunhee.newsimplememoapp.feature.R
 import com.hegunhee.newsimplememoapp.feature.databinding.FragmentAddMemoBinding
+import com.hegunhee.newsimplememoapp.feature.detailCategory.DetailCategoryFragment
+import com.hegunhee.newsimplememoapp.feature.detailCategory.DetailCategoryFragmentDirections
 import com.hegunhee.newsimplememoapp.feature.util.DateUtil
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -43,6 +46,11 @@ class AddMemoFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observeData()
+        setFragmentResultListener(DetailCategoryFragment.REFRESH_KEY) { key, bundle ->
+            if(key == DetailCategoryFragment.REFRESH_KEY) {
+                viewModel.refreshCategory()
+            }
+        }
     }
 
     private fun observeData()  {
@@ -58,6 +66,13 @@ class AddMemoFragment : Fragment(){
                             AddMemoState.SetDate -> setDate()
                             AddMemoState.SetTime -> setTime()
                             AddMemoState.SetPrice -> setPrice()
+                        }
+                    }
+                }
+                launch {
+                    viewModel.detailCategoryNavigation.collect {
+                        AddMemoFragmentDirections.addMemoToDetailCategory(it.code).also { direction ->
+                            findNavController().navigate(direction)
                         }
                     }
                 }
