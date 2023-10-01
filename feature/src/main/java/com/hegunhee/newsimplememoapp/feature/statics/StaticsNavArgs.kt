a9a3cc84b1fd46bc95159ca01ee1ba5b99e123a0
hegunhee <gunhee0072@gmail.com>
2023-10-01 09:47:00 +0900

    [statics-refactor] refactor : 통계 데이터 도메인모듈로 이동, 통계 상세로 전달하는 데이터 클래스 생성

--- feature/src/main/java/com/hegunhee/newsimplememoapp/feature/statics/StaticsNavArgs.kt
+++ feature/src/main/java/com/hegunhee/newsimplememoapp/feature/statics/StaticsNavArgs.kt
@@ -4,10 +4,8 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class StaticsData(
-    val percent: Int,
+data class StaticsNavArgs(
     val attr : String,
-    val price : Int,
     val year : Int,
     val month : Int
 ) : Parcelable
