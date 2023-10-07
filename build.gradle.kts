6904b131d51f7e883bd0cba8ae2616bddd69dbec
hegunhee <gunhee0072@gmail.com>
2023-10-07 13:49:06 +0900

    [build-logic] build : fragment-ktx 추가 및 hilt, gradle 버전 업

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:4.2.1")
+        classpath ("com.android.tools.build:gradle:7.4.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
 
