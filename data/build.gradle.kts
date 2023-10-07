37fbe848d6f02cb87a44886dbb2c330b7ed64f39
hegunhee <gunhee0072@gmail.com>
2023-10-07 14:39:56 +0900

    [build-logic] feat : data 모듈의 build.gradle에 build-logic 적용

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,25 +1,7 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id ("hegunhee.android")
     id ("kotlin-parcelize")
     id ("hegunhee.hilt")
-}
-
-android {
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 33
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
-    }
 }
 
 dependencies {
