77679e3783cfa5855ebd8b391cfd709a41228d08
hegunhee <gunhee0072@gmail.com>
2023-10-09 15:53:16 +0900

    [compose-navigation] feat : navController State 작성 및 NavHost 작성

--- compose-app/src/main/java/com/hegunhee/compose_app/MainActivity.kt
+++ compose-app/src/main/java/com/hegunhee/compose_app/MainActivity.kt
@@ -3,45 +3,12 @@ package com.hegunhee.compose_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.hegunhee.compose_app.ui.theme.NewSimpleMemoAppTheme
-import com.hegunhee.compose_feature.TestFeatureModule
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewSimpleMemoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    TestFeatureModule()
-                }
-            }
+            SimpleMemoApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewSimpleMemoAppTheme {
-        Greeting("Android")
     }
 }
---
+++ compose-app/src/main/java/com/hegunhee/compose_app/SimpleMemoApp.kt
@@ -0,0 +1,62 @@
+package com.hegunhee.compose_app
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.hegunhee.compose_app.ui.theme.NewSimpleMemoAppTheme
+import com.hegunhee.copose_memo.MemoNavGraph
+import com.hegunhee.copose_memo.memoNavGraph
+import com.hegunhee.statics.staticsNavGraph
+import kotlinx.coroutines.CoroutineScope
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleMemoApp(
+    simpleMemoAppScaffoldState: SimpleMemoAppScaffoldState = rememberSimpleMemoAppScaffoldState()
+) {
+    NewSimpleMemoAppTheme() {
+        Scaffold(bottomBar = { AppBottomNavigation(backStackEntry = simpleMemoAppScaffoldState.navController.currentBackStackEntryAsState(), onBottomClick = simpleMemoAppScaffoldState::navigateBottomNavigation)}) { paddingValues ->
+            NavHost(navController = simpleMemoAppScaffoldState.navController, startDestination = MemoNavGraph.memoRoute) {
+
+                memoNavGraph(paddingValues)
+
+                staticsNavGraph(paddingValues)
+            }
+        }
+    }
+}
+
+
+@Composable
+fun rememberSimpleMemoAppScaffoldState(
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController : NavHostController = rememberNavController()
+) : SimpleMemoAppScaffoldState {
+    return remember(key1 = coroutineScope, key2 = navController) {
+        SimpleMemoAppScaffoldState(
+            coroutineScope,
+            navController
+        )
+    }
+}
+
+class SimpleMemoAppScaffoldState(
+    val coroutineScope : CoroutineScope,
+    val navController : NavHostController
+) {
+    fun navigateBottomNavigation(index : Int) {
+        navController.navigate(bottomNavItems[index].screenRoute) {
+            navController.graph.startDestinationRoute?.let {
+                popUpTo(it) { saveState = true}
+            }
+            launchSingleTop = true
+            restoreState = true
+        }
+    }
+}
