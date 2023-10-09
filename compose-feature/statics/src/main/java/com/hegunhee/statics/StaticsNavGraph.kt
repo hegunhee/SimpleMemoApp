0a0a49e4f55871d9dc9ad467c0e59d4f4b38448a
hegunhee <gunhee0072@gmail.com>
2023-10-09 15:52:15 +0900

    [compose-navigation] feat : 메모 navGraph와 통계 navGraph 작성

---
+++ compose-feature/statics/src/main/java/com/hegunhee/statics/StaticsNavGraph.kt
@@ -0,0 +1,17 @@
+package com.hegunhee.statics
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+fun NavGraphBuilder.staticsNavGraph(
+    paddingValues: PaddingValues
+){
+    composable(route = StaticsNavGraph.statics) {
+        StaticsScreenRoot()
+    }
+}
+
+object StaticsNavGraph {
+    const val statics = "Statics"
+}
