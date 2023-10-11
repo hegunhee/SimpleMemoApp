31806040bc590c970eb60875461f68d787c63e1c
hegunhee <gunhee0072@gmail.com>
2023-10-11 17:36:11 +0900

    [compose-memo] feat : compose-app 모듈의 ui관련 파일들 패키지 이동 및 hilt 초기설정

---
+++ compose-app/src/main/java/com/hegunhee/compose_app/MemoApplication.kt
@@ -0,0 +1,8 @@
+package com.hegunhee.compose_app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MemoApplication : Application() {
+}
--- compose-feature/memo/src/main/java/com/hegunhee/copose_memo/MemoScreen.kt
+++ compose-feature/memo/src/main/java/com/hegunhee/copose_memo/MemoScreen.kt
@@ -1,20 +1,89 @@
 package com.hegunhee.copose_memo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hegunhee.compose_feature.memo.R
+import com.hegunhee.newsimplememoapp.core.ui.DateSelector
 
 @Composable
 fun MemoScreenRoot(
     paddingValues: PaddingValues
 ) {
-    MemoScreen(paddingValues = paddingValues)
-    Text(text = "Text 화면입니다.")
+    MemoScreen(
+        paddingValues = paddingValues,
+        year = 2023,
+        month =  10,
+        onPreviousMonthClick = {},
+        onNextMonthClick = {},
+        onSelectorClick = {}
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MemoScreen(
+    paddingValues: PaddingValues,
+    year : Int,
+    month : Int,
+    onPreviousMonthClick : () -> Unit,
+    onNextMonthClick : () -> Unit,
+    onSelectorClick : () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier
+            .padding(paddingValues)
+            .padding(top = 10.dp),
+        topBar = { DateSelector(
+            year = year,
+            month = month,
+            onPreviousMonthClick = onPreviousMonthClick,
+            onNextMonthClick = onNextMonthClick,
+            onSelectorClick = onSelectorClick
+        )},
+        floatingActionButton = {
+            FloatingActionButton(onClick = {  }) {
+                Icon(painter = painterResource(id = R.drawable.ic_add), contentDescription = "설명")
+            }
+        }
+    ) { padding ->
+        Column(modifier = Modifier.padding(padding)) {
+            TotalPriceGraph()
+
+        }
+    }
 }
 
 @Composable
-fun MemoScreen(
-    paddingValues: PaddingValues
-) {
-    
+private fun TotalPriceGraph() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Text(text = "수입", fontSize = 15.sp)
+        Text(text = "지출", fontSize = 15.sp)
+        Text(text = "합계", fontSize = 15.sp)
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Text(text = "123123", fontSize = 15.sp)
+        Text(text = "22222", fontSize = 15.sp)
+        Text(text = "222222", fontSize = 15.sp)
+    }
 }
