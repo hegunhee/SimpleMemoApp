3df3e2c65acb19f9324d35e05f9aa34a62dfa9d0
hegunhee <gunhee0072@gmail.com>
2023-10-12 20:34:16 +0900

    [compose-memo] feat : 바텀 네비게이션 바 visible 설정

--- compose-app/src/main/java/com/hegunhee/compose_app/ui/AppBottomNavigation.kt
+++ compose-app/src/main/java/com/hegunhee/compose_app/ui/AppBottomNavigation.kt
@@ -1,33 +1,33 @@
 package com.hegunhee.compose_app.ui
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavBackStackEntry
 
 @Composable
-fun AppBottomNavigation(backStackEntry: State<NavBackStackEntry?>,onBottomClick : (Int) -> Unit) {
-    BottomNavigation(
-        backgroundColor = Color.White,
-        contentColor = Color.Black
-    ) {
-        val currentRoute = backStackEntry.value?.destination?.route
-        bottomNavItems.forEachIndexed{ index, item ->
-            BottomNavigationItem(
-                label = { Text(text = item.title,fontSize = 9.sp)},
-                icon = { Icon(painter = painterResource(id = item.icon), contentDescription = item.title)},
-                selectedContentColor = Color.Magenta,
-                unselectedContentColor = Color.Black.copy(0.4f),
-                alwaysShowLabel = true,
-                selected = currentRoute == item.screenRoute,
-                onClick = {onBottomClick(index)}
-            )
+fun AppBottomNavigation(visible : Boolean, currentItem : BottomNavItem?, onBottomClick : (Int) -> Unit) {
+    AnimatedVisibility(visible = visible) {
+        BottomNavigation(
+            backgroundColor = Color.White,
+            contentColor = Color.Black
+        ) {
+            bottomNavItems.forEachIndexed{ index, item ->
+                BottomNavigationItem(
+                    label = { Text(text = item.title,fontSize = 9.sp)},
+                    icon = { Icon(painter = painterResource(id = item.icon), contentDescription = item.title)},
+                    selectedContentColor = Color.Magenta,
+                    unselectedContentColor = Color.Black.copy(0.4f),
+                    alwaysShowLabel = true,
+                    selected = item == currentItem,
+                    onClick = {onBottomClick(index)}
+                )
+            }
         }
     }
 }
