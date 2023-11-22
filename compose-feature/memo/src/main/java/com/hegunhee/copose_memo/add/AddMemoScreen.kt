3c667f2c0ff731443a493fb78b6cae05d84c61ca
hegunhee <gunhee0072@gmail.com>
2023-11-22 23:11:04 +0900

    [compose-memo] feat : AddMemoViewModel 생성

--- compose-feature/memo/src/main/java/com/hegunhee/copose_memo/add/AddMemoScreen.kt
+++ compose-feature/memo/src/main/java/com/hegunhee/copose_memo/add/AddMemoScreen.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.hegunhee.newsimplememoapp.core.ui.DetailMemoScreen
 import com.hegunhee.newsimplememoapp.core.ui.DetailMemoScreenType
 
 @Composable
 fun AddMemoScreenRoot(
     paddingValues : PaddingValues,
-    onBackButtonClick : () -> Unit
+    onBackButtonClick : () -> Unit,
+    viewModel : AddMemoViewModel = hiltViewModel()
 ) {
     val (category, setCategory) = remember{mutableStateOf("지출")}
     val memoScreenType = remember { DetailMemoScreenType.Add(onSaveMemoClick =  { })}
---
+++ compose-feature/memo/src/main/java/com/hegunhee/copose_memo/add/AddMemoViewModel.kt
@@ -0,0 +1,9 @@
+package com.hegunhee.copose_memo.add
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddMemoViewModel @Inject constructor() : ViewModel(){
+}
