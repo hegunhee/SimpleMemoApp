e0deed630073983070a973607d77e75f73bfc378
hegunhee <gunhee0072@gmail.com>
2023-11-30 22:24:59 +0900

    [compose-memo] feat : 카테고리를 추가하거나 삭제하고 돌아왔을때 바텀시트에 변경된 카테고리 반영

--- compose-feature/memo/src/main/java/com/hegunhee/copose_memo/add/AddMemoScreen.kt
+++ compose-feature/memo/src/main/java/com/hegunhee/copose_memo/add/AddMemoScreen.kt
@@ -2,6 +2,7 @@ package com.hegunhee.copose_memo.add
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -19,6 +20,9 @@ fun AddMemoScreenRoot(
     val (category, setCategory) = remember{mutableStateOf("지출")}
     val memoScreenType = remember { DetailMemoScreenType.Add(onSaveMemoClick =  { })}
 
+    LaunchedEffect(key1 = viewModel.categoryList) {
+        viewModel.setCategoryType(viewModel.subCategoryType.value)
+    }
     DetailMemoScreen(
         paddingValues = paddingValues,
         onBackButtonClick = onBackButtonClick,
