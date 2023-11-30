92217d72fa8cf563af7aecb0eb2dff9f1c61e630
hegunhee <gunhee0072@gmail.com>
2023-11-30 17:09:18 +0900

    [compose-memo] feat : Category BottomSheet 코드 다른 파일로 분리

---
+++ core/ui/src/main/java/com/hegunhee/newsimplememoapp/core/ui/CategoryBottomSheet.kt
@@ -0,0 +1,67 @@
+package com.hegunhee.newsimplememoapp.core.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hegunhee.newsimplememoapp.domain.model.CategoryType
+import com.holix.android.bottomsheetdialog.compose.BottomSheetDialog
+
+@Composable
+fun CategoryBottomSheet(
+    onDismissRequest : () -> Unit,
+    selectedCategoryType : CategoryType,
+    categoryList : List<String>,
+    onSubCategoryClick : (CategoryType) -> Unit,
+    onSubCategoryItemClick : (CategoryType, String) -> Unit
+) {
+
+    BottomSheetDialog(onDismissRequest = {
+        onDismissRequest()
+        onSubCategoryClick(CategoryType.Empty)
+    }) {
+        Surface {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = selectedCategoryType.title,modifier = Modifier.weight(0.8f).padding(start = 10.dp), fontSize = 20.sp)
+                    IconButton(onClick = {
+                        //TODO Detail Category로 이동
+                    }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_draw),contentDescription = null)
+                    }
+                    IconButton(onClick = {
+                        onDismissRequest()
+                        onSubCategoryClick(CategoryType.Empty)
+                    }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_exit),contentDescription = null)
+                    }
+                }
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(4)
+                ) {
+                    items(categoryList, key = { it }) { name ->
+                        Text(text = name,modifier = Modifier.clickable {
+                            onSubCategoryItemClick(selectedCategoryType,name)
+                            onDismissRequest
+                        }.padding(vertical = 10.dp),fontSize = 20.sp, textAlign = TextAlign.Center)
+                    }
+                }
+            }
+        }
+    }
+}
