cae659e0ebc4f199504c748274f6415967b0689c
hegunhee <gunhee0072@gmail.com>
2023-12-03 20:31:01 +0900

    [compose-statics] feat : StaticsScreen 뼈대 작성

--- compose-feature/statics/src/main/java/com/hegunhee/statics/StaticsScreen.kt
+++ compose-feature/statics/src/main/java/com/hegunhee/statics/StaticsScreen.kt
@@ -1,9 +1,17 @@
 package com.hegunhee.statics
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun StaticsScreenRoot() {
+fun StaticsScreenRoot(paddingValues: PaddingValues) {
+    StaticsScreen(paddingValues)
+}
+
+@Composable
+fun StaticsScreen(
+    paddingValues: PaddingValues
+) {
     Text(text = "Statics 화면입니다.")
 }
