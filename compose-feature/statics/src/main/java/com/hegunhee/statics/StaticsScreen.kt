7ca72aa07c52568cd408df0bd34b6fbb373f5a90
hegunhee <gunhee0072@gmail.com>
2023-12-04 13:49:37 +0900

    [compose-statics] feat : 통계 화면에서 날짜 선택할 수 있게 작성

--- compose-feature/statics/src/main/java/com/hegunhee/statics/StaticsScreen.kt
+++ compose-feature/statics/src/main/java/com/hegunhee/statics/StaticsScreen.kt
@@ -1,17 +1,62 @@
 package com.hegunhee.statics
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.hegunhee.newsimplememoapp.core.ui.DatePickerDialog
+import com.hegunhee.newsimplememoapp.core.ui.DateSelector
+import com.hegunhee.newsimplememoapp.domain.model.StaticsData
 
 @Composable
 fun StaticsScreenRoot(paddingValues: PaddingValues) {
-    StaticsScreen(paddingValues)
+    StaticsScreen(
+        paddingValues
+    )
 }
 
 @Composable
 fun StaticsScreen(
-    paddingValues: PaddingValues
+    paddingValues: PaddingValues,
+    year : Int,
+    month : Int,
+    staticsList : List<StaticsData>,
+    onPreviousMonthCLick : () -> Unit,
+    onNextMonthClick : () -> Unit,
+    onDatePickerCurrentMonthClick : () -> Unit,
+    onDatePickerMonthClick : (Int, Int) -> Unit,
 ) {
-    Text(text = "Statics 화면입니다.")
+    var datePickerDialogState by remember{ mutableStateOf<Boolean>(false) }
+    val showDatePickerDialog = { datePickerDialogState = true}
+    val dismissDatePickerDialog = { datePickerDialogState = false}
+
+    if(datePickerDialogState) {
+        DatePickerDialog(
+            initialYear = year,
+            onDismissDialog = dismissDatePickerDialog,
+            onCurrentMonthClick = onDatePickerCurrentMonthClick,
+            onMonthClick = onDatePickerMonthClick
+        )
+    }
+    Scaffold(
+        modifier = Modifier
+            .padding(paddingValues)
+            .padding(top = 10.dp),
+        topBar = { DateSelector(
+            year = year,
+            month = month,
+            onPreviousMonthClick = onPreviousMonthCLick,
+            onNextMonthClick = onNextMonthClick ,
+            onSelectorClick = showDatePickerDialog
+        ) }
+    ) { padding ->
+        Text(text = staticsList.toString(),modifier = Modifier.padding(padding))
+    }
 }
---
+++ compose-feature/statics/src/main/java/com/hegunhee/statics/StaticsViewModel.kt
@@ -0,0 +1,4 @@
+package com.hegunhee.statics
+
+class StaticsViewModel {
+}
