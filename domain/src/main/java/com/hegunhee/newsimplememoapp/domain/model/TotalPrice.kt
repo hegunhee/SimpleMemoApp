c1a555a5539e62865b9ba6892e92c026f947cbc9
hegunhee <gunhee0072@gmail.com>
2023-12-11 17:38:26 +0900

    [refactor] refactor : 기존의 data, domain모듈의 코드 삭제

--- domain/src/main/java/com/hegunhee/newsimplememoapp/domain/model/TotalPrice.kt
+++
@@ -1,7 +0,0 @@
-package com.hegunhee.newsimplememoapp.domain.model
-
-data class TotalPrice(
-    val incomePrice : Int,
-    val expensePrice : Int,
-    val sumPrice : Int = incomePrice - expensePrice
-)
