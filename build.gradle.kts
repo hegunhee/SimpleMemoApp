0b9dbdf21a86702819791ef3f5fafe15393ef428
hegunhee <gunhee0072@gmail.com>
2024-08-27 15:20:16 +0900

    [local-to-server] build : 네트워크 통신을 위한 Retrofit라이브러리, 직렬화 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,14 @@ buildscript {
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
+//        classpath ("org.jetbrains.kotlin.plugin.serialization:1.8.10")/
+
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
+}
+plugins {
+    id ("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
 task("clean",Delete::class) {
