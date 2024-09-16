4da3f6cd960bf1d15bb5f0756a00d70da7ca275c
hegunhee <gunhee0072@gmail.com>
2024-09-16 11:22:18 +0900

    [compose-server] feat : Version O버전 코드 삭제

--- core/util/src/main/java/com/hegunhee/newsimplememoapp/util/DateUtil.kt
+++ core/util/src/main/java/com/hegunhee/newsimplememoapp/util/DateUtil.kt
@@ -1,105 +1,25 @@
 package com.hegunhee.newsimplememoapp.util
 
-import android.os.Build
-import com.hegunhee.newsimplememoapp.domain.model.DateInfo
-import com.hegunhee.newsimplememoapp.domain.model.TimeInfo
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.util.Calendar
-import java.util.Date
 
 object DateUtil {
 
-    private val calendar = Calendar.getInstance().apply {
-        time = Date()
-    }
-
     private fun getKoreaLocalTime() = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
 
-    private val koreanDayOfWeek = listOf<String>("일","월","화","수","목","금","토")
-
-    fun getYear() : Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            getKoreaLocalTime().year
-        }else {
-            calendar.get(Calendar.YEAR)
-        }
+    fun getYear(): Int {
+        return getKoreaLocalTime().year
     }
 
-    fun getMonth() : Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            getKoreaLocalTime().monthValue
-        }else {
-            calendar.get(Calendar.MONTH) + 1
-        }
-    }
-    fun getDayOfMonth(): Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            getKoreaLocalTime().dayOfMonth
-        }else {
-            calendar.get(Calendar.DAY_OF_MONTH)
-        }
-    }
-    fun getDayOfWeek(year : Int,month : Int,day : Int) : String {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            LocalDate.of(year, month, day).dayOfWeek.toString().changeKoreanDayOfWeek()
-        } else {
-            val diffCalendar = Calendar.getInstance().apply {
-                set(year,month-1,day)
-            }
-            return koreanDayOfWeek[diffCalendar.get(Calendar.DAY_OF_WEEK)]
-        }
+    fun getMonth(): Int {
+        return getKoreaLocalTime().monthValue
     }
 
-    private fun String.changeKoreanDayOfWeek(): String =
-        when (this) {
-            "MONDAY" -> "월"
-            "TUESDAY" -> "화"
-            "WEDNESDAY" -> "수"
-            "THURSDAY" -> "목"
-            "FRIDAY" -> "금"
-            "SATURDAY" -> "토"
-            "SUNDAY" -> "일"
-            else -> "error"
-        }
-
-    fun getHour() : Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            getKoreaLocalTime().hour
-        }else {
-            calendar.get(Calendar.HOUR_OF_DAY)
-        }
+    fun getHour(): Int {
+        return getKoreaLocalTime().hour
     }
 
-    fun getMinute() : Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            getKoreaLocalTime().minute
-        }else {
-            calendar.get(Calendar.MINUTE)
-        }
-    }
-
-    fun getTodayDate() : DateInfo {
-        val year = getYear()
-        val month = getMonth()
-        val dayOfMonth = getDayOfMonth()
-        val dayOfWeek = getDayOfWeek(year,month,dayOfMonth)
-        return DateInfo(year,month,dayOfMonth,dayOfWeek)
-    }
-
-    fun getTodayTime() : TimeInfo {
-        val hour = if(getHour() > 12) {
-            getHour() - 12
-        }else {
-            getHour()
-        }
-        val minute = getMinute()
-        val ampm = if(getHour() <= 12) {
-            "오전"
-        }else {
-            "오후"
-        }
-        return TimeInfo(hour,minute,ampm)
+    fun getMinute(): Int {
+        return getKoreaLocalTime().minute
     }
 }
