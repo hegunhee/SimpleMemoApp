f6197fb85640c9f640b38de9623f15878a461477
hegunhee <gunhee0072@gmail.com>
2024-09-16 11:25:15 +0900

    [compose-server] feat : LocalDB 코드 걷어내기

--- core/data/src/main/java/com/hegunhee/newsimplememoapp/data/di/DefaultDataSourceModule.kt
+++ core/data/src/main/java/com/hegunhee/newsimplememoapp/data/di/DefaultDataSourceModule.kt
@@ -1,8 +1,6 @@
 package com.hegunhee.newsimplememoapp.data.di
 
-import com.hegunhee.newsimplememoapp.data.dataSource.DefaultLocalDataSource
 import com.hegunhee.newsimplememoapp.data.dataSource.DefaultRemoteDataSource
-import com.hegunhee.newsimplememoapp.data.dataSource.LocalDataSource
 import com.hegunhee.newsimplememoapp.data.dataSource.RemoteDataSource
 import dagger.Binds
 import dagger.Module
@@ -13,12 +11,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 abstract class DefaultDataSourceModule {
-
-    @Singleton
-    @Binds
-    abstract fun provideDefaultLocalDataSource(
-        defaultLocalDataSource: DefaultLocalDataSource
-    ) : LocalDataSource
 
     @Singleton
     @Binds
